cd5bf24f3fe4fb66d4be6da363fb6b8191124e2d
dadhireddy sathish <93584413+sathish-git-hub@users.noreply.github.com>
2024-10-18 19:32:14 +0530

    Delete old-challenges directory

--- old-challenges/challenge-2-CI-CD/app/routes/root.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res, next) {
-  res.contentType = "text/html";
-  res.end('<html><body body style="background-color:powderblue;">  <p align="center"><font size="6">Hello Kubernetes!</font></p></body></html>');
-  next();
-};
